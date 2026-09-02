a16291dd630b8d4b3e97ae483f4531f400c4b15b
Himanshu <himanshubansal2608@gmail.com>
2026-09-02 11:08:20 +0530

    path added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
-app.use(express.static("public"));
+let pathToFolder = path.join(__dirname, "public");
+
+app.use(express.static(pathToFolder));
 
 app.listen(3000);
